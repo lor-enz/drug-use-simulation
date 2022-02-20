a59e221699c1acac4319c05ac3c24bc5ae5f44df
lor-enz <git@lorenz.kiwi>
2022-02-20 18:54:59 +0100

    Remove many tests from test_simulation

--- code/test_simulation.py
+++ code/test_simulation.py
@@ -9,7 +9,7 @@ class TestSimAndEva(unittest.TestCase):
         from country import Country
         germany = Country("germany")
         agents = AgentFactory(how_many_agents,germany).agents
-        sim = Simulation(agents)
+        sim = Simulation(agents, label=f"{how_many_agents} Agents in germany")
         print("SETUP done")
         return sim
 
@@ -32,53 +32,9 @@ class TestSimAndEva(unittest.TestCase):
         self.assertEqual(result['alive'], 9)
         self.assertEqual(result['dead'], 1)
 
-    def test_small_pop(self):
-        from agent_factory import AgentFactory
-        from country import Country
-        germany = Country("germany")
-        agents = AgentFactory(100,germany).agents
-        agents[0].alive = False
-        import evaluation
-        result = evaluation.evaluate(agents)
-        print(result)
-
-    def test_small_simu(self):
-        import random
-        random.seed(10)
-        import evaluation
-        sim = self.create_sim(300)
-        previous = evaluation.evaluate(sim.agents)
-        print(f"previous: {previous}")
-        for i in range(50):
-            sim.run(1)
-            result = evaluation.evaluate(sim.agents)
-            print(f"after: {result}")
-
-
-    def test_small_simu2(self):
-        import random
-        random.seed(10)
-        import evaluation
-        sim = self.create_sim(300)
-        previous = evaluation.evaluate(sim.agents)
-        print(f"previous: {previous}")
-        sim.run(50)
-        result = evaluation.evaluate(sim.agents)
-        print(f"after: {result}")
-
-    def test_with_simu(self):
-        import evaluation
-        sim1000 = self.create_sim(10000)
-        previous = evaluation.evaluate(sim1000.agents)
-        print(f"previous: {previous}")
-        for i in range(1):
-            sim1000.run(100)
-            result = evaluation.evaluate(sim1000.agents)
-            print(f"after: {result}")
-
     def test_simulation(self):
-        sim = self.create_sim(1000)
-        sim.run(100)
+        sim = self.create_sim(2000)
+        sim.run(52)
 
 
 if __name__ == "__main__":
