2929e9981cad3de643d52b3190f49ae2ab3b54f6
lor-enz <git@lorenz.kiwi>
2022-02-22 11:20:22 +0100

    Refactor country.py

--- code/country.py
+++ code/country.py
@@ -1,68 +1,66 @@
-class Country():
-    germany = "germany"
-    portugal = "portugal"
-    cannabis_addict_rate_m = 0
-    cannabis_addict_rate_f = 0
-    cocaine_addict_rate_m = 0
-    cocaine_addict_rate_f = 0
-    mdma_addict_rate_m = 0
-    mdma_addict_rate_f = 0
-    amphetamines_addict_rate_m = 0
-    amphetamines_addict_rate_f = 0
-    opioid_addict_rate_m = 0
-    opioid_addict_rate_f = 0
+germany = {
+    "citizen_number_f": 42109535,
+    "citizen_number_m": 41011828,
+    "citizens": 83121363,
+    "law_level": 3,
 
-    # 3 law-categories
-    drug_acceptance = 0
-    law_level = 1
+    # https://www.emcdda.europa.eu/system/files/publications/11334/germany-cdr-2019_0.pdf
+    "cannabis_addict_rate_m": 0.156,
+    "cannabis_addict_rate_f": 0.11,
+    "cocaine_addict_rate_m": 0.013,
+    "cocaine_addict_rate_f": 0.011,
+    "mdma_addict_rate_m": 0.013,
+    "mdma_addict_rate_f": 0.013,
+    "amphetamines_addict_rate_m": 0.019,
+    "amphetamines_addict_rate_f": 0.019,
 
-    citizens = 1
-    citizen_number_f = 1
-    citizen_number_m = 1
+    # https://www.bundesgesundheitsministerium.de/fileadmin/Dateien/5_Publikationen/Drogen_und_Sucht/Berichte/AbschlussberichtOpiS-Bericht_150518.pdf
+    "opioids_addict_rate_m_dividend": 122968,
+    "opioids_addict_rate_f_dividend": 41826
+}
 
-    def __init__(self, country):
-        if country == self.germany:
-            # https://www.destatis.de/DE/Themen/Gesellschaft-Umwelt/Bevoelkerung/Bevoelkerungsstand/Tabellen/liste-zensus-geschlecht-staatsangehoerigkeit.html
-            self.citizen_number_f = 42109535
-            self.citizen_number_m = 41011828
-            self.citizens = 83121363
+portugal = {
+    "citizen_number_f": 5423632,
+    "citizen_number_m": 4921170,
+    "citizens": 10344802,
+    "law_level": 1,
 
-            self.law_level = 3
+    # https://www.emcdda.europa.eu/system/files/publications/11334/germany-cdr-2019_0.pdf
+    "cannabis_addict_rate_m": 0.109,
+    "cannabis_addict_rate_f": 0.05,
+    "cocaine_addict_rate_m": 0.004,
+    "cocaine_addict_rate_f": 0.003,
+    "mdma_addict_rate_m": 0.004,
+    "mdma_addict_rate_f": 0.001,
+    "amphetamines_addict_rate_m": 0.0,
+    "amphetamines_addict_rate_f": 0.0,
 
-            # https://www.emcdda.europa.eu/system/files/publications/11334/germany-cdr-2019_0.pdf
-            self.cannabis_addict_rate_m = 0.156
-            self.cannabis_addict_rate_f = 0.11
-            self.cocaine_addict_rate_m = 0.013
-            self.cocaine_addict_rate_f = 0.011
-            self.mdma_addict_rate_m = 0.013
-            self.mdma_addict_rate_f = 0.013
-            self.amphetamines_addict_rate_m = 0.019
-            self.amphetamines_addict_rate_f = 0.019
+    # https://www.bundesgesundheitsministerium.de/fileadmin/Dateien/5_Publikationen/Drogen_und_Sucht/Berichte/AbschlussberichtOpiS-Bericht_150518.pdf
+    "opioids_addict_rate_m_dividend": 33290,
+    "opioids_addict_rate_f_dividend": 33290
+}
 
-            # https://www.bundesgesundheitsministerium.de/fileadmin/Dateien/5_Publikationen/Drogen_und_Sucht/Berichte/AbschlussberichtOpiS-Bericht_150518.pdf
-            self.opioids_addict_rate_m = 122968 / self.citizen_number_m
-            self.opioids_addict_rate_f = 41826 / self.citizen_number_f
-        if country == self.portugal:
-            # https://www.ine.pt/xportal/xmain?xpid=INE&xpgid=ine_indicadores&contecto=pi&indOcorrCod=0011166&selTab=tab0
-            self.citizen_number_f = 5423632
-            self.citizen_number_m = 4921170
-            self.citizens = 10344802
 
-            self.law_level = 1
+class Country:
 
-            # https://www.emcdda.europa.eu/system/files/publications/11331/portugal-cdr-2019_0.pdf
-            self.cannabis_addict_rate_m = 0.109
-            self.cannabis_addict_rate_f = 0.05
-            self.cocaine_addict_rate_m = 0.004
-            self.cocaine_addict_rate_f = 0.003
-            self.mdma_addict_rate_m = 0.004
-            self.mdma_addict_rate_f = 0.001
-            self.amphetamines_addict_rate_m = 0.0
-            self.amphetamines_addict_rate_f = 0.0
+    def __init__(self, countr_dict):
+        self.citizen_number_f = countr_dict['citizen_number_f']
+        self.citizen_number_m = countr_dict['citizen_number_m']
+        self.citizens = countr_dict['citizens']
 
-            # only height risk number avaible, not devided by gender
-            self.opioids_addict_rate_m = 33290 / self.citizens
-            self.opioids_addict_rate_f = 33290 / self.citizens
+        self.law_level = countr_dict['law_level']
+
+        self.cannabis_addict_rate_m = countr_dict['cannabis_addict_rate_m']
+        self.cannabis_addict_rate_f = countr_dict['cannabis_addict_rate_f']
+        self.cocaine_addict_rate_m = countr_dict['cocaine_addict_rate_m']
+        self.cocaine_addict_rate_f = countr_dict['cocaine_addict_rate_f']
+        self.mdma_addict_rate_m = countr_dict['mdma_addict_rate_m']
+        self.mdma_addict_rate_f = countr_dict['mdma_addict_rate_f']
+        self.amphetamines_addict_rate_m = countr_dict['amphetamines_addict_rate_m']
+        self.amphetamines_addict_rate_f = countr_dict['amphetamines_addict_rate_f']
+
+        self.opioids_addict_rate_m = countr_dict['opioids_addict_rate_m_dividend'] / self.citizen_number_m
+        self.opioids_addict_rate_f = countr_dict['opioids_addict_rate_f_dividend'] / self.citizen_number_f
+
         rate_sum = self.cannabis_addict_rate_m + self.cannabis_addict_rate_f + self.cocaine_addict_rate_m + self.cocaine_addict_rate_f + self.mdma_addict_rate_m + self.mdma_addict_rate_f + self.amphetamines_addict_rate_m + self.amphetamines_addict_rate_f + self.opioids_addict_rate_m + self.opioids_addict_rate_f
         self.drug_acceptance = (rate_sum / 10) / self.law_level
-
